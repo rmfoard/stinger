11f6009d8f50e6119dff993f17c6270a94651341
Richard Foard <richard.foard@gtri.gatech.edu>
2016-12-05 17:10:26 -0500

    Changed community detector to return internal and external edge weight sums

--- lib/stinger_alg/src/community_on_demand.c
+++ lib/stinger_alg/src/community_on_demand.c
@@ -6,25 +6,28 @@
  *  On-demand community detection
  *
  */
-int64_t community_on_demand(const stinger_t * S, int64_t ** vertices, int64_t ** partitions, double_t ** parmods) {
-  if (*vertices != NULL || *partitions != NULL || *parmods != NULL) {
+int64_t community_on_demand(const stinger_t * S, int64_t ** vertices, int64_t ** partitions, int64_t ** intsums, int64_t ** extsums) {
+  if (*vertices != NULL || *partitions != NULL || *intsums != NULL || *extsums != NULL) {
     LOG_E("Community on demand output arrays should not be allocated before call.  Possible memory leak.");
   }
 
   int64_t * output_vertices = NULL;
   int64_t * output_partitions = NULL;
-  double_t * output_parmods = NULL;
+  int64_t * output_intsums = NULL;
+  int64_t * output_extsums = NULL;
 
   /* Initialize to NULL in case of error */
   *vertices = output_vertices;
   *partitions = output_partitions;
-  *parmods = output_parmods;
+  *intsums = output_intsums;
+  *extsums = output_extsums;
 
   /* Allocate output array */
   int64_t nv = stinger_max_active_vertex(S) + 1;
   output_vertices = (int64_t *) xmalloc (nv * sizeof(int64_t));
   output_partitions = (int64_t *) xmalloc (nv * sizeof(int64_t));
-  output_parmods = (double_t *) xmalloc(nv * sizeof(double_t));
+  output_intsums = (int64_t *) xmalloc(nv * sizeof(int64_t));
+  output_extsums = (int64_t *) xmalloc(nv * sizeof(int64_t));
 
   /* Initialize vertex numbers */
   int64_t v;
@@ -33,12 +36,13 @@ int64_t community_on_demand(const stinger_t * S, int64_t ** vertices, int64_t **
   }
 
   /* Note wiring of max iterations to 5 */
-  community_detection(S, nv, output_partitions, output_parmods, (int64_t) 5);
+  community_detection(S, nv, output_partitions, output_intsums, output_extsums, (int64_t) 5);
 
   /* Set output arrays */
   *vertices = output_vertices;
   *partitions = output_partitions;
-  *parmods = output_parmods;
+  *intsums = output_intsums;
+  *extsums = output_extsums;
 
   return nv;
 }
--- lib/stinger_alg/src/modularity.c
+++ lib/stinger_alg/src/modularity.c
@@ -65,20 +65,20 @@ louvain_method(stinger_t * S, int64_t * partitions, int64_t size, int64_t m, int
 /**
  * Return the ratio of v's partition's internal to external edge weight sums
  */
-double_t
-int_ext_wt_ratio(stinger_t * S, int64_t NV, int64_t * partitions, double_t * parmod, int64_t v) {
+void
+sum_wts(stinger_t * S, int64_t NV, int64_t * partitions, int64_t *pintsum, int64_t *pextsum, int64_t v) {
     // Sum internal and external weights for each vertex in v's partition
     int64_t v_partition = partitions[v];
-    double_t internal_wt = 0.0;
-    double_t external_wt = 0.0;
+    *pintsum = 0;
+    *pextsum = 0;
     // for outward and...
     for (int64_t i = 0; i < NV; i += 1) if (partitions[i] == v_partition) {
         STINGER_FORALL_OUT_EDGES_OF_VTX_BEGIN(S, i)
         {
             if (partitions[STINGER_EDGE_DEST] == v_partition) {
-                internal_wt += STINGER_EDGE_WEIGHT;
+                *pintsum += STINGER_EDGE_WEIGHT;
             } else {
-                external_wt += STINGER_EDGE_WEIGHT;
+                *pextsum += STINGER_EDGE_WEIGHT;
             }
         }
         STINGER_FORALL_OUT_EDGES_OF_VTX_END();
@@ -88,18 +88,12 @@ int_ext_wt_ratio(stinger_t * S, int64_t NV, int64_t * partitions, double_t * par
         STINGER_FORALL_IN_EDGES_OF_VTX_BEGIN(S, i)
         {
             if (partitions[STINGER_EDGE_SOURCE] == v_partition) {
-                internal_wt += STINGER_EDGE_WEIGHT;
+                *pintsum += STINGER_EDGE_WEIGHT;
             } else {
-                external_wt += STINGER_EDGE_WEIGHT;
+                *pextsum += STINGER_EDGE_WEIGHT;
             }
         }
         STINGER_FORALL_IN_EDGES_OF_VTX_END();
-    }
-
-    if (external_wt > 0.0) {
-        return internal_wt/external_wt;
-    } else {
-        return 0.0001;
     }
 }
 
@@ -111,27 +105,35 @@ int_ext_wt_ratio(stinger_t * S, int64_t NV, int64_t * partitions, double_t * par
  * partitions, and a the maximum number of iterations the algorithm will run for
  */
 void
-community_detection(stinger_t * S, int64_t NV, int64_t * partitions, double_t * parmod, int64_t maxIter) {
+community_detection(stinger_t * S, int64_t NV, int64_t * partitions, int64_t * intsums, int64_t * extsums, int64_t maxIter) {
     // We need the sum of the total weights in the graph to calculate modularity
     double_t m = 0;
     STINGER_FORALL_EDGES_OF_ALL_TYPES_BEGIN(S) {
                             m += STINGER_EDGE_WEIGHT;
                         } STINGER_FORALL_EDGES_OF_ALL_TYPES_END();
 
-    // Begin by setting each vertex to its own partiton, parmod[] to -1
+    // Begin by setting each vertex to its own partiton, sums to -1
     for (int64_t i = 0; i < NV; i++) {
         partitions[i] = i;
-        parmod[i] = (double_t) -1.0;
+        intsums[i] = -1;
+        extsums[i] = -1;
     }
 
     // Assign each vertex to a community
     partitions = louvain_method(S, partitions, NV, m, maxIter);
 
-    // Store the connectedness of each community in association with its vertices.
+    // Store the internal and external edge weight sums of each community in association with its vertices.
+    int64_t isum;
+    int64_t esum;
     for (int64_t i = 0; i < NV; i += 1) {
-        if (parmod[i] < 0.0) {  // if we haven't already seen this vertex's partition
-            double_t connectedness = int_ext_wt_ratio(S, NV, partitions, parmod, i);
-            for (int64_t j = 0; j < NV; j += 1) if (partitions[j] == i) parmod[j] = connectedness;
+        if (intsums[i] < 0) {  // if we haven't already seen this vertex's partition
+            sum_wts(S, NV, partitions, &isum, &esum, i);
+            for (int64_t j = 0; j < NV; j += 1) {
+                if (partitions[j] == i) {
+                    intsums[j] += isum;
+                    extsums[j] += esum;
+                }
+            }
         }
     }
 }
